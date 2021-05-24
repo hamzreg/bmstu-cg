82b31e5106009757cc008ba277935af93cdffe18
khrr19u863 <regina.hamsina@gmail.com>
2021-05-24 23:43:32 +0300

    FIX: colour constants, delete comments

--- lab_06/main.py
+++ lab_06/main.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 import tkinter as tk
 from tkinter import messagebox
-import numpy as np
 
+import numpy as np
 import time
 
 @dataclass
@@ -24,11 +24,23 @@ class Graphic:
 
 @dataclass
 class Colour:
+    """
+        Цвета.
+    """
 
     back = "#ABCDEF"
     draw = "#3E8353"
     button = "#DDF1FF"
     text = "#28852F"
+
+    black = "#000000"
+    white = "#FFFFFF"
+    red = "#FF0000"
+    green = "#00FF00"
+    blue = "#0000FF"
+
+TYPE_PAUSE = 1
+TYPE_NOT_PAUSE = 2
 
 
 # Заполнение
@@ -41,6 +53,7 @@ def create_time(work_time):
                                 bg = Colour.back,
                                 width=40)
     time_etr.place(x = 50, y = 50)
+
     time_str = "Время заполнения: {0:6.4f}".format(work_time)
     time_etr.insert(tk.END, time_str)
 
@@ -61,17 +74,22 @@ def seed_fill(stack, colour, pause):
 
         x, y = now_point[0] + 1, now_point[1]
 
-        while image_canvas.get(x, y) != edge_colour and image_canvas.get(x, y) != colour[0]:
+        while (image_canvas.get(x, y) != edge_colour and 
+              image_canvas.get(x, y) != colour[0]):
             image_canvas.put(colour[1], (x, y))
             x += 1
+
         rx = x - 1
-        image_canvas.put(colour[1], (now_point[0] + 1, y, rx + 1, y + 1))
+        image_canvas.put(colour[1], 
+                        (now_point[0] + 1, y, rx + 1, y + 1))
 
         x = now_point[0] - 1
 
-        while image_canvas.get(x, y) != edge_colour and image_canvas.get(x, y) != colour[0]:
+        while (image_canvas.get(x, y) != edge_colour and 
+               image_canvas.get(x, y) != colour[0]):
             image_canvas.put(colour[1], (x, y))
             x -= 1
+
         lx = x + 1
         image_canvas.put(colour[1], (lx, y, now_point[0], y + 1))
 
@@ -81,7 +99,8 @@ def seed_fill(stack, colour, pause):
 
             while x <= rx:
                 flag = 0
-                while image_canvas.get(x, y) != edge_colour and image_canvas.get(x, y) != colour[0] and x <= rx:
+                while (image_canvas.get(x, y) != edge_colour and 
+                       image_canvas.get(x, y) != colour[0] and x <= rx):
                     flag = 1
                     x += 1
 
@@ -90,7 +109,10 @@ def seed_fill(stack, colour, pause):
 
                     flag = 0
                 xi = x
-                while (image_canvas.get(x, y) == edge_colour or image_canvas.get(x, y) == colour[0]) and x < rx:
+
+                while ((image_canvas.get(x, y) == edge_colour 
+                        or image_canvas.get(x, y) == colour[0]) 
+                        and x < rx):
                     x += 1
 
                 if x == xi:
@@ -113,16 +135,23 @@ def fill():
     last = dots_box.get(dots_box.size() - 1)
 
     if last != "     ":
-        messagebox.showerror("Ошибка", "Нужно замкнуть фигуру перед заполнением.")
+        messagebox.showerror("Ошибка",
+                             "Нужно замкнуть фигуру перед заполнением.")
+        return
+
+    
+    if len(stack) != 1:
+        messagebox.showerror("Ошибка",
+                             "Затравочный пиксель должен быть задан.")
         return
 
     colour = get_colour()
     type_id = get_type()
 
-    if type_id == 1:
+    if type_id == TYPE_PAUSE:
         pause_time = get_pause_time()
         seed_fill(stack, colour, pause_time)
-    elif type_id == 2:
+    elif type_id == TYPE_NOT_PAUSE:
         seed_fill(stack, colour, 0)
 
 
@@ -141,7 +170,7 @@ def clear():
                                  height = Graphic.canvas_heigth)
     canvas.create_image((Graphic.canvas_width / 2, Graphic.canvas_heigth / 2), 
                         image = image_canvas, state = "normal")
-    image_canvas.put("#ffffff", to = (0, 0, Graphic.canvas_width, Graphic.canvas_heigth))
+    image_canvas.put(Colour.white, to = (0, 0, Graphic.canvas_width, Graphic.canvas_heigth))
     
     dots_box.delete(0, dots_box.size())
     points.clear()
@@ -162,11 +191,11 @@ def get_colour():
     colour = colour_list.get(index)
 
     if colour == "Синий":
-        return [(0, 0, 255), "#0000FF"]
+        return [(0, 0, 255), Colour.blue]
     elif colour == "Зелёный":
-        return [(0, 255, 0), "#00FF00"]
+        return [(0, 255, 0), Colour.green]
     elif colour == "Красный":
-        return [(255, 0, 0), "#FF0000"]
+        return [(255, 0, 0), Colour.red]
 
 # Тип
 def get_type():
@@ -179,9 +208,9 @@ def get_type():
     select = fill_list.get(index)
 
     if select == "С задержкой":
-        return 1
+        return TYPE_PAUSE
     elif select == "Без задержки":
-        return 2
+        return TYPE_NOT_PAUSE
 
 
 # Время задержки
@@ -207,7 +236,7 @@ def draw_edge(edge):
     """
 
     for point in edge:
-        image_canvas.put("#000000", (point[0], point[1]))
+        image_canvas.put(Colour.black, (point[0], point[1]))
 
 
 def brezenham_int(start, end):
@@ -260,8 +289,10 @@ def close_figure():
     """
 
     if len(points[-1]) < 3:
-        messagebox.showerror("Ошибка", "Чтобы замкнуть фигуру необходимо минимум 3 точки.")
+        messagebox.showerror("Ошибка",
+                             "Чтобы замкнуть фигуру необходимо минимум 3 точки.")
         return
+
     if len(points[-1]) > 1:
         start = points[-1][-1]
         end = points[-1][0]
@@ -273,8 +304,11 @@ def close_figure():
 
 
 # Работа с точками
-
 def draw_point(x, y, color):
+    """
+        Рисование точки.
+    """
+
     image_canvas.put(color, (x, y))
 
 
@@ -330,12 +364,10 @@ def add_seed_dot(x, y):
         затравочного пикселя.
     """
 
-    stack.append([x, y])
+    if len(stack) == 1:
+        stack.clear()
 
-    #if len(stack) == 1:
-    #     draw_point(stack[0][0], stack[0][1], "white")
-    # colour = get_colour()
-    # draw_point(x, y, colour)
+    stack.append([x, y])
 
 
 def get_seed_dot():
@@ -358,7 +390,8 @@ def get_seed_dot():
 def get_seed_dot(event):
     """
         Получение координат 
-        затравочного пикселя.
+        затравочного пикселя,
+        задаваемого мышкой.
     """
 
     x = event.x 
@@ -387,12 +420,14 @@ if __name__ == "__main__":
 
     canvas = tk.Canvas(window, width = Graphic.canvas_width,
                                height = Graphic.canvas_heigth,
-                               bg = "white")
+                               bg = Colour.white)
     canvas.place(x = 50, y = 150)
 
-    image_canvas = tk.PhotoImage(width = Graphic.canvas_width, height = Graphic.canvas_heigth)
-    canvas.create_image((Graphic.canvas_width / 2, Graphic.canvas_heigth / 2), image = image_canvas, state = "normal")
-    image_canvas.put("#ffffff", to = (0, 0, Graphic.canvas_width, Graphic.canvas_heigth))
+    image_canvas = tk.PhotoImage(width = Graphic.canvas_width,
+                                 height = Graphic.canvas_heigth)
+    canvas.create_image((Graphic.canvas_width / 2, Graphic.canvas_heigth / 2),
+                         image = image_canvas, state = "normal")
+    image_canvas.put(Colour.white, to = (0, 0, Graphic.canvas_width, Graphic.canvas_heigth))
 
     # Время
     time_etr = tk.Entry(window, font = Graphic.font_bold,
@@ -401,7 +436,6 @@ if __name__ == "__main__":
     time_etr.place(x = 50, y = 50)
 
     # Добавление точки
-
     point_lbl = tk.Label(window, text = "Добавление точки",
                                  font = Graphic.font_bold,
                                  bg = Colour.back)
@@ -436,7 +470,6 @@ if __name__ == "__main__":
     add_point_btn.place(x = 1960, y = 140) 
 
     # Список точек
-
     dots_lbl = tk.Label(window, text = "Список точек",
                                  font = Graphic.font_bold,
                                  bg = Colour.back)
@@ -457,7 +490,6 @@ if __name__ == "__main__":
     close_fig_btn.place(x = 1960, y = 720)
 
     # Затравочный пиксель
-
     seed_point_lbl = tk.Label(window, text = "Затравочный пиксель",
                                  font = Graphic.font_bold,
                                  bg = Colour.back)
@@ -486,7 +518,6 @@ if __name__ == "__main__":
     seed_y_etr.place(x = 2652, y = 860)
 
     # Выбор цвета
-
     colour_lbl = tk.Label(window, text = "Цвет заполнения",
                                   font = Graphic.font_bold,
                                   bg = Colour.back)
@@ -504,7 +535,6 @@ if __name__ == "__main__":
         colour_list.insert(tk.END, colour)
 
     # Выбор типа заполнения
-
     fill_lbl = tk.Label(window, text = "Тип заполнения",
                                   font = Graphic.font_bold,
                                   bg = Colour.back)
@@ -541,7 +571,6 @@ if __name__ == "__main__":
     fill_btn.place(x = 1960, y = 1360) 
 
     # Очистить экран
-
     clear_btn = tk.Button(window, font = Graphic.font,
                                   width = 38,
                                   bg = Colour.button,
